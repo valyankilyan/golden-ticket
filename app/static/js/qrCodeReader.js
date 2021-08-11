6222be7b09e4ca2f39a120609861a0b15c259031
valyankilyan <valentin.kil.2002@gmail.com>
2021-08-12 02:47:31 +0300

    scanner is working

---
+++ app/static/js/qrCodeReader.js
@@ -0,0 +1,61 @@
+const QRcode = window.qrcode;
+
+const video = document.createElement("video");
+const canvasElement = document.getElementById("qr-canvas");
+const canvas = canvasElement.getContext("2d");
+
+const qrResult = document.getElementById("qr-result");
+const outputData = document.getElementById("outputData");
+const btnScanQR = document.getElementById("btn-scan-qr");
+
+let scanning = false;
+
+QRcode.callback = (res) => {
+    if (res) {
+        outputData.innerText = res;
+        scanning = false;
+
+        video.srcObject.getTracks().forEach(track => {
+            track.stop();
+        });
+
+        qrResult.hidden = false;
+        btnScanQR.hidden = false;
+        canvasElement.hidden = true;
+    }
+};
+
+btnScanQR.onclick = () => {
+    navigator.mediaDevices
+        .getUserMedia({ video: { facingMode: "environment" } })
+        .then(function (stream) {
+            scanning = true;
+            qrResult.hidden = true;
+            btnScanQR.hidden = true;
+            canvasElement.hidden = false;
+            video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+            video.srcObject = stream;
+            video.play();
+            tick();
+            scan();
+        });
+};
+
+function tick() {
+    console.log("tick")
+    canvasElement.height = video.videoHeight;
+    canvasElement.width = video.videoWidth;
+    canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+
+    scanning && requestAnimationFrame(tick);
+}
+
+
+function scan() {
+    console.log("scan")
+    try {
+        QRcode.decode();
+    } catch (e) {
+        setTimeout(scan, 100);
+    }
+}
